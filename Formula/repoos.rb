ce7a95f07fb9b9a652cbd0c8bbdb34ca87773a4c
Nick Jachowski <njachowski@gmail.com>
2026-09-17 18:59:25 +0800

    feat: add repoos formula

---
+++ Formula/repoos.rb
@@ -0,0 +1,21 @@
+class Repoos < Formula
+  desc "Repo-native task tracking for AI-assisted engineering"
+  homepage "https://repoos.org"
+  url "https://registry.npmjs.org/@repo-os/repoos/-/repoos-0.5.46.tgz"
+  sha256 "25530e7fffbd7307ff4b0d431289499f7567edc474c3a3e698d64d553a7c2011"
+  license "FSL-1.1-MIT"
+
+  depends_on "node"
+
+  def install
+    package_root = libexec/"node_modules/@repo-os/repoos"
+    package_root.install Dir["*"]
+
+    (bin/"repoos").write_env_script package_root/"dist/cli/index.js",
+                                      PATH: "#{formula_opt_bin("node")}:$PATH"
+  end
+
+  test do
+    assert_match "repoos v#{version}", shell_output("#{bin}/repoos --version")
+  end
+end
